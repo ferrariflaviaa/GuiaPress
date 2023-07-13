2376764d72556353c0fafab14da1f98c8d309aea
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-07-13 16:59:48 -0300

    feat: category form

--- controller/categories/CategoriesController.js
+++ controller/categories/CategoriesController.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-  res.send("Rota de categoria")
-})
-
 router.get("/admin/categories/new", (req, res) => {
-  res.send("rota para criar uma nova categoria")
+  res.render("admin/categories/new")
 })
 
 module.exports = router;
