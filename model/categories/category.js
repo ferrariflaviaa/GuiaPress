8b6cf1cac8c0aec1e24a8656274af33202799ba3
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-07-13 11:24:56 -0300

    feat: category model

---
+++ model/categories/category.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require('sequelize')
+const connection = require("../../database/database")
+
+const Category = connection.define('categories', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }, slug: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+})
+
+module.exports = Category;
