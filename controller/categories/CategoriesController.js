21e726370f417019adac435ee6c5353c6356c410
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-07-13 17:25:38 -0300

    feat: category registration and Slugify

--- controller/categories/CategoriesController.js
+++ controller/categories/CategoriesController.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const slugify = require("slugify")
 const router = express.Router();
+const Category = require("./../../model/categories/category")
 
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new")
+})
+
+router.post("/categories/save", (req, res) => {
+  let title = req.body.title;
+  if (title != undefined) {
+    Category.create({
+      title: title,
+      slug: slugify(title)
+    }).then(() => {
+      res.redirect("/")
+    })
+  } else {
+    res.redirect("/admin/categories/new");
+  }
 })
 
 module.exports = router;
