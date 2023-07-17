f2c51a316b9791f0cbbe1438cb0feceff1f3c54b
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-07-17 13:52:06 -0300

    feat: saving edit

--- controller/categories/CategoriesController.js
+++ controller/categories/CategoriesController.js
@@ -49,17 +49,30 @@ router.post("/categories/delete", (req, res) => {
 router.get("/admin/categories/edit/:id", (req, res) => {
   let id = req.params.id;
 
-  if(isNaN(id)){
+  if (isNaN(id)) {
     res.redirect("/admin/categories")
   }
 
   Category.findByPk(id).then(category => {
     if (category != undefined) {
-      res.render("admin/categories/edit", {category: category})
+      res.render("admin/categories/edit", { category: category })
     } else {
       res.redirect("/admin/categories")
     }
   }).catch(() => {
+    res.redirect("/admin/categories")
+  })
+})
+
+router.post("/categories/update", (req, res) => {
+  let id = req.body.id;
+  let title = req.body.title;
+
+  Category.update({ title: title, slug: slugify(title) }, {
+    where: {
+      id: id
+    }
+  }).then(() => {
     res.redirect("/admin/categories")
   })
 })
