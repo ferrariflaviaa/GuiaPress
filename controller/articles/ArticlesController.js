50e437fac6251c537b22ae4af7979022adc7f539
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-07-17 15:32:48 -0300

    feat: categories dropdown

--- controller/articles/ArticlesController.js
+++ controller/articles/ArticlesController.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../../model/categories/category")
 
 router.get("/articles", (req, res) => {
   res.send("Rota de artigos")
 })
 
 router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new")
+  Category.findAll().then(categories => {
+    res.render("admin/articles/new", { categories: categories })
+  })
 })
 
 router.get
