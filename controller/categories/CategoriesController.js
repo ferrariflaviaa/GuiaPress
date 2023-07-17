2654ede2d4fc87b4d2cc87590c4bc6e9a962929f
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-07-17 11:39:59 -0300

    feat: edit form

--- controller/categories/CategoriesController.js
+++ controller/categories/CategoriesController.js
@@ -14,7 +14,7 @@ router.post("/categories/save", (req, res) => {
       title: title,
       slug: slugify(title)
     }).then(() => {
-      res.redirect("/")
+      res.redirect("/admin/categories")
     })
   } else {
     res.redirect("/admin/categories/new");
@@ -44,6 +44,24 @@ router.post("/categories/delete", (req, res) => {
   } else { // NULL
     res.redirect("/admin/categories")
   }
+})
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+  let id = req.params.id;
+
+  if(isNaN(id)){
+    res.redirect("/admin/categories")
+  }
+
+  Category.findByPk(id).then(category => {
+    if (category != undefined) {
+      res.render("admin/categories/edit", {category: category})
+    } else {
+      res.redirect("/admin/categories")
+    }
+  }).catch(() => {
+    res.redirect("/admin/categories")
+  })
 })
 
 module.exports = router;
