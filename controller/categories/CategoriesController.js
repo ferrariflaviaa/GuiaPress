bdb30c1aaf1d775b73e11e03a60da148c3164723
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-07-17 10:35:57 -0300

    feat: deleting category

--- controller/categories/CategoriesController.js
+++ controller/categories/CategoriesController.js
@@ -21,11 +21,29 @@ router.post("/categories/save", (req, res) => {
   }
 })
 
-router.get("/admin/categories", (req, res)=> {
+router.get("/admin/categories", (req, res) => {
   Category.findAll().then(categories => {
-    res.render("admin/categories/index", {categories: categories})
+    res.render("admin/categories/index", { categories: categories })
   })
 })
 
+router.post("/categories/delete", (req, res) => {
+  let id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Category.destroy({
+        where: {
+          id: id
+        }
+      }).then(() => {
+        res.redirect("/admin/categories")
+      })
+    } else { //NÃO FOR NÚMERO
+      res.redirect("/admin/categories")
+    }
+  } else { // NULL
+    res.redirect("/admin/categories")
+  }
+})
 
 module.exports = router;
