f9bcd4d562a83072741ba0e8362425d7779d3e7f
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-07-18 09:49:32 -0300

    feat: table of articles

--- controller/articles/ArticlesController.js
+++ controller/articles/ArticlesController.js
@@ -6,7 +6,9 @@ const slugify = require("slugify")
 
 
 router.get("/admin/articles", (req, res) => {
-  res.render("admin/articles/index");
+  Article.findAll().then(articles => {
+    res.render("admin/articles/index", { articles: articles });
+  })
 })
 
 router.get("/admin/articles/new", (req, res) => {
