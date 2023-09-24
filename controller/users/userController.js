2bfe062c5866a9851e0396f2cc17ed663e189c6c
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-09-24 18:07:17 -0300

    feat: preventing duplicate emails

--- controller/users/userController.js
+++ controller/users/userController.js
@@ -5,7 +5,7 @@ const User = require("./../../model/users/users")
 
 router.get("/admin/users", (req, res) => {
   User.findAll().then(users => {
-    res.render("admin/users/index",{users: users})
+    res.render("admin/users/index", { users: users })
   })
 })
 
@@ -15,15 +15,24 @@ router.get("/admin/users/create", (req, res) => {
 
 router.post("/users/create", (req, res) => {
   let { email, password } = req.body
-  let salt = bcrypt.genSaltSync(10)
-  let hash = bcrypt.hashSync(password, salt)
 
-  User.create({
-    email,
-    password: hash
-  }).then(() => {
-    res.redirect("/")
+  User.findOne({ where: { email: email } }).then(user => {
+    console.log(user)
+    if (user == undefined) {
+      let salt = bcrypt.genSaltSync(10)
+      let hash = bcrypt.hashSync(password, salt)
+
+      User.create({
+        email,
+        password: hash
+      }).then(() => {
+        res.redirect("/")
+      })
+    } else {
+      res.redirect("/admin/users/create")
+    }
   })
+
 
 })
 module.exports = router;
