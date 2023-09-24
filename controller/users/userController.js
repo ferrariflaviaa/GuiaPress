1067da152b6fb1bcab6c83a8dbb91d8e23b85ae7
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-09-24 17:58:44 -0300

    feat: user list

--- controller/users/userController.js
+++ controller/users/userController.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const User = require("./../../model/users/users")
 
 router.get("/admin/users", (req, res) => {
-  res.send("Listagem de usuários")
+  User.findAll().then(users => {
+    res.render("admin/users/index",{users: users})
+  })
 })
 
 router.get("/admin/users/create", (req, res) => {
