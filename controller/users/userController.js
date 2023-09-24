28c10274d309fe51f5c3faa8299e2e4f3646628e
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-09-24 17:12:14 -0300

    feat: registration form

--- controller/users/userController.js
+++ controller/users/userController.js
@@ -7,7 +7,14 @@ router.get("/admin/users", (req, res) => {
   res.send("Listagem de usuários")
 })
 
-router.get("/admin/users/create", (req,res) => {
+router.get("/admin/users/create", (req, res) => {
   res.render("admin/users/create")
+})
+
+router.post("/users/create", (req, res) => {
+  let email = req.body.email
+  let password = req.body.password
+
+  console.log( email, password )
 })
 module.exports = router;
